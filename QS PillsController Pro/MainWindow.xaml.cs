580a3db6557f8889525baf3a263896a27144be77
Alrighter <59889888+Alrighter@users.noreply.github.com>
2021-03-11 00:51:37 +0200

    Initial commit

---
+++ QS PillsController Pro/MainWindow.xaml.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MaterialDesignThemes.Wpf;
+
+namespace QS_PillsController_Pro
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected override void OnPropertyChanged(DependencyPropertyChangedEventArgs e)
+        {
+            if (_frequency ==  1)
+            {
+                PresetTimePicker1.IsEnabled = true;
+                PresetTimePicker2.IsEnabled = false;
+                PresetTimePicker3.IsEnabled = false;
+            }
+            else if (_frequency == 2)
+            {
+                PresetTimePicker1.IsEnabled = true;
+                PresetTimePicker2.IsEnabled = true;
+                PresetTimePicker3.IsEnabled = false;
+            }
+            else if (_frequency == 3)
+            {
+                PresetTimePicker1.IsEnabled = true;
+                PresetTimePicker2.IsEnabled = true;
+                PresetTimePicker3.IsEnabled = true;
+            }
+            
+            base.OnPropertyChanged(e);
+        }
+
+        public MainWindow()
+        { 
+            InitializeComponent();
+        }
+
+        #region Fields
+
+        private string _pillName;
+        private int _frequency;
+        private DateTime _startDateTime;
+        private DateTime _endDateTime;
+        private string _time1;
+        private string _time2;
+        private string _time3;
+
+        #endregion
+
+
+        #region Properties
+
+        public string PillName
+        {
+            get { return _pillName;}
+            set { _pillName = value; }
+        }
+
+        public int Frequency
+        {
+            get { return _frequency; }
+            set { _frequency = value; }
+        }
+
+        public DateTime StartDateTime
+        {
+            get { return _startDateTime; }
+            set { _startDateTime = value; }
+        }
+
+        public DateTime EndDateTime
+        {
+            get { return _endDateTime; }
+            set { _endDateTime = value; }
+        }
+
+        public string Time1
+        {
+            get { return _time1; }
+            set { _time1 = value; }
+        }
+
+        public string Time2
+        {
+            get { return _time2; }
+            set { _time2 = value; }
+        }
+
+        public string Time3
+        {
+            get { return _time3; }
+            set { _time3 = value; }
+        }
+
+        #endregion
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+
+            if (_pillName != null & _frequency != null & _startDateTime != null & _endDateTime != null & _time1 != null)
+            {
+                SQLiteConnection sqLite_connection = CreateConnection();
+                CreateTable(sqLite_connection);
+                InsertData(sqLite_connection, PillName, Convert.ToString(StartDateTime), Convert.ToString(EndDateTime), Frequency);
+                MessageBox.Show(_time1);
+            }
+            else
+            {
+                MessageBox.Show("Введите корректные данные. " + _time1);
+            }
+            
+
+            
+
+
+        }
+
+        #region SQLite
+
+        public SQLiteConnection CreateConnection()
+        {
+            SQLiteConnection sqlconn = new SQLiteConnection("Data Source=database.db; Version = 3; New = True; Compress = True; ");
+            try
+            {
+                sqlconn.Open();
+            }
+            catch (Exception Ex)
+            {
+                MessageBox.Show("Error: " + Convert.ToString(Ex));
+            }
+            return sqlconn;
+        }
+
+        public void CreateTable(SQLiteConnection conn)
+        {
+
+            SQLiteCommand sqlcmd;
+            string createTableCmd = "CREATE TABLE IF NOT EXISTS Information(ID INTEGER NOT NULL UNIQUE, NAME TEXT NOT NULL,START_DATE TEXT NOT NULL, END_DATE TEXT NOT NULL,FREQUENCY INTEGER NOT NULL, PRIMARY KEY(ID AUTOINCREMENT)); ";
+            sqlcmd = conn.CreateCommand();
+
+            sqlcmd.CommandText = createTableCmd;
+            sqlcmd.ExecuteNonQuery();
+
+        }
+
+        public void InsertData(SQLiteConnection conn, string name, string startDate, string endDate, int frequency)
+        {
+            SQLiteCommand sqlcmd = new SQLiteCommand($"INSERT INTO Information (NAME, START_DATE, END_DATE, FREQUENCY) VALUES('{name}', '{startDate}', '{endDate}', {frequency});", conn);
+            sqlcmd.ExecuteNonQuery();
+        }
+
+        #endregion
+
+        #region HelpButtons
+
+        private void NameTBox_OnMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            MessageBox.Show("Введите название, которое будет отображаться в напоминании.");
+        }
+
+        private void FrequencyTBox_OnMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            MessageBox.Show("Выберите как часто вы будете принимать таблетки за сутки.");
+
+        }
+
+        private void HistoryButton_OnMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Window _window1;
+            _window1 = new History();
+            _window1.Show();
+        }
+
+        #endregion
+
+
+
+    }
+}
+
---
+++ QS PillsController Pro/TaskCreator.cs
@@ -0,0 +1,268 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Win32.TaskScheduler;
+
+namespace QS_PillsController_Pro
+{
+    class TaskCreator
+    {
+        #region fields
+        private string _pillName;
+        private int _frequency;
+        DateTime _startDate;
+        DateTime _endDate;
+        private int _duration;
+        private int _time1hr;
+        private int _time1min;
+        private int _time2hr;
+        private int _time2min;
+        private int _time3hr;
+        private int _time3min;
+        #endregion
+
+
+        public TaskCreator(string PillName, int frequency, DateTime StartDate, DateTime EndDate, string Time1)
+        {
+            #region fields
+
+            _pillName = PillName;
+            _frequency = frequency;
+            _startDate = StartDate;
+            _endDate = EndDate;
+            char h1 = Time1[0];
+            char h2 = Time1[1];
+            _time1hr = Convert.ToInt32(h1) + Convert.ToInt32(h2);
+            char m1 = Time1[3];
+            char m2 = Time1[4];
+            _time1min = Convert.ToInt32(m1) + Convert.ToInt32(m2);
+            #endregion
+
+            #region TaskCreatorScript
+
+            for (DateTime a = _startDate; a.Day < _endDate.Day; a.AddDays(1))
+            {
+
+                DateTime myDate = ChangeTime(a, _time1hr, _time1min);
+
+                using (TaskService ts = new TaskService())
+                {
+
+                    // Create a new task definition and assign properties
+                    TaskDefinition td = ts.NewTask();
+                    td.RegistrationInfo.Description = $"{_pillName} trigger";
+
+                    // Create a trigger that will fire the task at this time every other day
+                    td.Triggers.Add(new TimeTrigger(myDate));
+
+                    // Create an action that will launch Notepad whenever the trigger fires
+                    td.Actions.Add(new ExecAction("notepad.exe", null, null));
+
+                    // Register the task in the root folder
+                    ts.RootFolder.RegisterTaskDefinition($@"{_pillName}", td);
+
+                    // Remove the task we just created
+                    ts.RootFolder.DeleteTask("Test");
+                }
+
+                
+            }
+
+            #endregion
+        }
+
+        public TaskCreator(string PillName, int frequency, DateTime StartDate, DateTime EndDate, string Time1, string Time2)
+        {
+            #region fields
+
+            _pillName = PillName;
+            _frequency = frequency;
+            _startDate = StartDate;
+            _endDate = EndDate;
+            char h1 = Time1[0];
+            char h2 = Time1[1];
+            _time1hr = Convert.ToInt32(h1) + Convert.ToInt32(h2);
+            char m1 = Time1[3];
+            char m2 = Time1[4];
+            _time1min = Convert.ToInt32(m1) + Convert.ToInt32(m2);
+            h1 = Time2[0];
+            h2 = Time2[1];
+            _time2hr = Convert.ToInt32(h1) + Convert.ToInt32(h2);
+            m1 = Time2[3];
+            m2 = Time2[4];
+            _time2min = Convert.ToInt32(m1) + Convert.ToInt32(m2);
+            #endregion
+
+            #region TaskCreatorScript
+
+            for (DateTime a = _startDate; a.Day < _endDate.Day; a.AddDays(1))
+            {
+
+                DateTime myDate = ChangeTime(a, _time1hr, _time1min);
+                using (TaskService ts = new TaskService())
+                {
+
+                    // Create a new task definition and assign properties
+                    TaskDefinition td = ts.NewTask();
+                    td.RegistrationInfo.Description = $"{_pillName} trigger";
+
+                    // Create a trigger that will fire the task at this time every other day
+                    td.Triggers.Add(new TimeTrigger(myDate));
+
+                    // Create an action that will launch Notepad whenever the trigger fires
+                    td.Actions.Add(new ExecAction("notepad.exe", "c:\\test.log", null));
+
+                    // Register the task in the root folder
+                    ts.RootFolder.RegisterTaskDefinition($@"{_pillName}", td);
+
+                    // Remove the task we just created
+                    ts.RootFolder.DeleteTask("Test");
+                }
+
+                myDate = ChangeTime(a, _time2hr, _time2min);
+
+                using (TaskService ts = new TaskService())
+                {
+                    
+                    // Create a new task definition and assign properties
+                    TaskDefinition td = ts.NewTask();
+                    td.RegistrationInfo.Description = $"{_pillName} trigger";
+
+                    // Create a trigger that will fire the task at this time every other day
+                    td.Triggers.Add(new TimeTrigger(myDate));
+
+                    // Create an action that will launch Notepad whenever the trigger fires
+                    td.Actions.Add(new ExecAction("notepad.exe", "c:\\test.log", null));
+
+                    // Register the task in the root folder
+                    ts.RootFolder.RegisterTaskDefinition($@"{_pillName}", td);
+
+                    // Remove the task we just created
+                    ts.RootFolder.DeleteTask("Test");
+                }
+
+            }
+
+            #endregion
+        }
+
+        public TaskCreator(string PillName, int frequency, DateTime StartDate, DateTime EndDate, string Time1, string Time2, string Time3)
+        {
+            #region fields
+
+            _pillName = PillName;
+            _frequency = frequency;
+            _startDate = StartDate;
+            _endDate = EndDate;
+            char h1 = Time1[0];
+            char h2 = Time1[1];
+            _time1hr = Convert.ToInt32(h1) + Convert.ToInt32(h2);
+            char m1 = Time1[3];
+            char m2 = Time1[4];
+            _time1min = Convert.ToInt32(m1) + Convert.ToInt32(m2);
+            h1 = Time2[0];
+            h2 = Time2[1];
+            _time2hr = Convert.ToInt32(h1) + Convert.ToInt32(h2);
+            m1 = Time2[3];
+            m2 = Time2[4];
+            _time2min = Convert.ToInt32(m1) + Convert.ToInt32(m2);
+            h1 = Time3[0];
+            h2 = Time3[1];
+            _time3hr = Convert.ToInt32(h1) + Convert.ToInt32(h2);
+            m1 = Time3[3];
+            m2 = Time3[4];
+            _time3min = Convert.ToInt32(m1) + Convert.ToInt32(m2);
+            #endregion
+
+            #region TaskCreatorScript
+
+            for (DateTime a = _startDate; a.Day < _endDate.Day; a.AddDays(1))
+            {
+
+                DateTime myDate = ChangeTime(a, _time1hr, _time1min);
+                using (TaskService ts = new TaskService())
+                {
+
+                    // Create a new task definition and assign properties
+                    TaskDefinition td = ts.NewTask();
+                    td.RegistrationInfo.Description = $"{_pillName} trigger";
+
+                    // Create a trigger that will fire the task at this time every other day
+                    td.Triggers.Add(new TimeTrigger(myDate));
+
+                    // Create an action that will launch Notepad whenever the trigger fires
+                    td.Actions.Add(new ExecAction("notepad.exe", "c:\\test.log", null));
+
+                    // Register the task in the root folder
+                    ts.RootFolder.RegisterTaskDefinition($@"{_pillName}", td);
+
+                    // Remove the task we just created
+                    ts.RootFolder.DeleteTask("Test");
+                }
+
+                myDate = ChangeTime(a, _time2hr, _time2min);
+
+                using (TaskService ts = new TaskService())
+                {
+
+                    // Create a new task definition and assign properties
+                    TaskDefinition td = ts.NewTask();
+                    td.RegistrationInfo.Description = $"{_pillName} trigger";
+
+                    // Create a trigger that will fire the task at this time every other day
+                    td.Triggers.Add(new TimeTrigger(myDate));
+
+                    // Create an action that will launch Notepad whenever the trigger fires
+                    td.Actions.Add(new ExecAction("notepad.exe", "c:\\test.log", null));
+
+                    // Register the task in the root folder
+                    ts.RootFolder.RegisterTaskDefinition($@"{_pillName}", td);
+
+                    // Remove the task we just created
+                    ts.RootFolder.DeleteTask("Test");
+                }
+
+                myDate = ChangeTime(a, _time3hr, _time3min);
+
+                using (TaskService ts = new TaskService())
+                {
+
+                    // Create a new task definition and assign properties
+                    TaskDefinition td = ts.NewTask();
+                    td.RegistrationInfo.Description = $"{_pillName} trigger";
+
+                    // Create a trigger that will fire the task at this time every other day
+                    td.Triggers.Add(new TimeTrigger(myDate));
+
+                    // Create an action that will launch Notepad whenever the trigger fires
+                    td.Actions.Add(new ExecAction("notepad.exe", "c:\\test.log", null));
+
+                    // Register the task in the root folder
+                    ts.RootFolder.RegisterTaskDefinition($@"{_pillName}", td);
+
+                    // Remove the task we just created
+                    ts.RootFolder.DeleteTask("Test");
+                }
+
+            }
+
+            #endregion
+        }
+        
+        public static DateTime ChangeTime(DateTime dateTime, int hours, int minutes)
+        {
+            return new DateTime(
+                dateTime.Year,
+                dateTime.Month,
+                dateTime.Day,
+                hours,
+                minutes,
+                00,
+                00,
+                dateTime.Kind);
+        }
+    }
+}
